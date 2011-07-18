6d908590a6ed5acb3f15ed4ddb823b74e0dbb3ac
gacek47 <gacek47@gmail.com>
2011-07-18 19:38:28 +0200

    def logo w ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 module ApplicationHelper
+  # Zwraca obrazek - logo lub tekst alternatywny
+  def logo
+	obraz = image_tag("logo.png", :alt => "Sample App", :class => "round")
+  end
+  
   # Return a title on a per-page basis.
   def title
     base_title = "Ruby on Rails Tutorial Sample App"
