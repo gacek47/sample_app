813984ff96dc8965f05702431673ac6fe1e57ac4
gacek47 <gacek47@gmail.com>
2011-07-18 18:58:51 +0200

    Zakonczono 'layouts' i 'routes' r. 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 SampleApp::Application.routes.draw do
   
+  get "users/new"
+
   # routes wskazane bezposrednio; potrzebne poprzednio dla pages-controller
   # get "pages/home"
   # get "pages/contact"
@@ -11,6 +13,7 @@ SampleApp::Application.routes.draw do
     match '/contact', :to => 'pages#contact'
     match '/about',   :to => 'pages#about'
     match '/help',    :to => 'pages#help'
+	match '/signup',  :to => 'users#new'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
